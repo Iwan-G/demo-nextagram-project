5cb540fada9bbb1bf8f85d5c1e14a54955fd2911
Iwan-G <74765961+Iwan-G@users.noreply.github.com>
2020-12-11 18:07:18 +0800

    First commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Card, CardImg, CardBody, CardTitle, CardText
+  Card, CardImg, CardBody, CardTitle, CardText, Button
 } from 'reactstrap';
 
 import UserImages from '../containers/UserImages';
@@ -14,29 +14,28 @@ const HomePage = ({users}) => {
       <div className ="d-flex flex-wrap" style={{margin:"10px"}}>
           {users.map(user => {
               return (
-                  <div key={user.id} className ="row d-flex flex-wrap" style={{backgroundColor:"lightgray", marginBottom:"10px", display:"flex"}}>
-                      
-                          <Card className ="col-3 d-flex align-items-center" style={{width:"25vw",margin:"0px", borderStyle:"none", backgroundColor:"lightgray", textAlign:"center"}}>
-          
-                              <CardImg className="rounded-circle"top width="100%" src={user.profileImage} alt="Card image cap" style={{width:"80%", border:"4px solid white", marginTop:"15px"}} />
-                              <CardBody >
-                              <CardTitle>
-                                  <Link to={`/profile/${user.id}`}>{user.username}</Link>
-        
-                                  </CardTitle>
-                              
-                              <CardText>ID: {user.id}</CardText>
-                              
-                              
-                              </CardBody>
-          
-                          </Card>
+                <div key={user.id} className ="row d-flex flex-wrap" style={{backgroundColor:"lightgray", marginBottom:"10px"}}>
+                        
+                <Card className ="col-3 d-flex align-items-center" style={{width:"25vw",margin:"0px", borderStyle:"none", backgroundColor:"lightgray", textAlign:"center"}}>
 
-                      <div className ="col-9 d-flex flex-wrap" style={{paddingLeft:"10px", display:"flex"}}>
-                          <UserImages userId ={user.id}/>
-                      </div>
-                  
-                  </div>
+                    <CardImg className="rounded-circle"top width="100%" src={user.profileImage} alt="Card image cap" style={{width:"80%", border:"4px solid white", marginTop:"15px"}} />
+                    <CardBody >
+                    <CardTitle>{user.username}</CardTitle>
+                    
+                    <CardText>ID: {user.id}</CardText>
+                    <Link to={`/users/${user.id}`}>
+                        <Button>View Profile</Button>
+                    </Link>
+                    
+                    </CardBody>
+
+                </Card>
+
+            <div className ="col-9 d-flex flex-wrap" style={{paddingLeft:"10px"}}>
+                <UserImages userId ={user.id}/>
+            </div>
+        
+        </div>
               )
           })}
       </div>
